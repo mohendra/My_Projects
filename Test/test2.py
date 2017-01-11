7592e48317ee298d881a639272bec1daaded649a
Mohendra <mohendra.roy@gmail.com>
2017-01-11 13:20:29 +0530

    updated

--- Test/test2.py
+++ Test/test2.py
@@ -55,5 +55,64 @@ def make_label(data):
 
 
 
+# function to calculate the outcome
+
+def outcome(data):
+  Last_coulm_of_data = [row[-1] for row in data]
+  max_outcome = max(set(Last_coulm_of_data), key=Last_coulm_of_data.count)
+
+  return max_outcome
+
+# function to divide the node
+
+def split_node(node, maximum_depth, minimum_sample, innitial_depth):
+
+    left, right = node['groups']
+
+    del(node['groups'])
+
+    if not left or not right:
+
+        node['left'] = node['right'] = outcome(left + right)
+        return
+
+    if innitial_depth >= maximum_depth:
+
+        node['left'], node['right'] = outcome(left), outcome(right)
+        return
+
+    if len(left) < minimum_sample:
+        node['left'] = outcome(left)
+    else:
+        node['left'] = make_label(left)
+        split_node(node['left'], maximum_depth, minimum_sample, innitial_depth+1)
+
+    if len(right) < minimum_sample:
+        node['right'] = outcome(right)
+    else:
+        node['right'] = make_label(right)
+        split_node(node['right'], maximum_depth, minimum_sample, innitial_depth+1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
