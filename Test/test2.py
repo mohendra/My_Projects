cea62a9a1ade7183700d3e63b4b0a48303186ff3
Mohendra <mohendra.roy@gmail.com>
2017-01-13 14:26:40 +0530

    updated

--- Test/test2.py
+++ Test/test2.py
@@ -1,7 +1,7 @@
 # definition/function for the split the data
 
 def data_split(data, index, value):
-    left = right = list()
+    left, right = list(), list()
     for row in data:
         if row[index] < value:
             left.append(row)
@@ -25,7 +25,7 @@ def gini_index(groups, class_values):
 
             p = [row[-1] for row in g].count(c)/float(size)
 
-            gini += p*(1.0 -p)
+            gini += p*p*(1.0 -p)*(1.0 -p)
 
     return gini
 
@@ -81,29 +81,65 @@ def split_node(node, maximum_depth, minimum_sample, innitial_depth):
         node['left'], node['right'] = outcome(left), outcome(right)
         return
 
-    if len(left) < minimum_sample:
+    if len(left) <= minimum_sample:
         node['left'] = outcome(left)
     else:
         node['left'] = make_label(left)
         split_node(node['left'], maximum_depth, minimum_sample, innitial_depth+1)
 
-    if len(right) < minimum_sample:
+    if len(right) <= minimum_sample:
         node['right'] = outcome(right)
     else:
         node['right'] = make_label(right)
         split_node(node['right'], maximum_depth, minimum_sample, innitial_depth+1)
 
+# function to finally building tree
+
+def build_tree(data, maximum_depth, minimum_sample):
+    root = make_label(data)
+    split_node(root, maximum_depth, minimum_sample, 1)
+    return root
+
+def ptree(node):
+    if isinstance(node, dict):
+        print 'at', node['index'], 'for value',  node['value']
+        ptree(node['left'])
+        ptree(node['right'])
+    else:
+        print [node]
+
+
+data =[[10,10,0],
+   [20,10,0],
+   [30,10,0],
+   [40,10,0],
+   [50,10,0],
+   [60,10,0],
+   [70,20,2],
+   [80,20,2],
+   [90,20,2],
+   [100,20,2],
+   [100,20,2],
+   [200,20,0,1],
+   [200,10,1],
+   [201,10,1],
+   [300,10,1],
+   [150,10,1],
+   [150,10,1],
+   [160,10,1],
+   [170,20,2],
+   [180,20,2],
+   [190,20,2],
+   [120,20,2],
+   [110,20,2],
+   [111,20,2,]]
 
 
 
+tree=build_tree(data, 2, 1)
 
 
-
-
-
-
-
-
+ptree(tree)
 
 
 
