a45a38171ec51c707d9ef3dd3dba3097c790a673
Mohendra <mohendra.roy@gmail.com>
2017-01-21 09:32:14 +0530

    random forest test

---
+++ Test/test6.py
@@ -0,0 +1,23 @@
+from random import seed
+from random import randrange
+from csv import reader
+from math import sqrt
+
+# function for importing and open csv file
+
+def opencsv(filename):
+    dataset=list() # this is ap
+    with open(filename,'r') as file:
+
+        csv_read=read(file)
+
+        for row in csv_read:
+            if not row:
+                continue
+            dataset.append(row)
+
+    return dataset
+
+filename='sonar.all-data.csv'
+
+opencsv(filename)
