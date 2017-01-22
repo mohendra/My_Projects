1dd48e13e413ac8229b84a5e3cdaf7e17e227a85
Mohendra <mohendra.roy@gmail.com>
2017-01-22 14:47:04 +0530

    Random forent till load_file for loading CSV file

--- Test/test5.py
+++ Test/test5.py
@@ -1,18 +1,32 @@
 from random import seed
 from random import randrange
 from math import sqrt
+from csv import reader
 
-# function for loading the csv file
+#function for loading csv file
 
-def load_csv(filename):
+def load_file(filename):
+
     dataset=list()
+
     with open(filename,'r') as file:
-        csv_reader=read(file)
-        for row in csv_reader:
+
+        data=reader(file)
+        for row in data:
+
             if not row:
                 continue
+
             dataset.append(row)
+
     return dataset
 
+# we will now test the function
+
+file='/Users/mohendra/Dropbox/My_Projects/Test/sd.csv'
+
+L=load_file(file)
+
+print L
 
 
