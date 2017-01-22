38cc39e3e444be35fcf74f9c0a69087e8f3793de
Mohendra <mohendra.roy@gmail.com>
2017-01-22 14:47:43 +0530

    Random forent till load_file for loading CSV file

--- Test/test6.py
+++ Test/test6.py
@@ -5,21 +5,26 @@ from math import sqrt
 
 # function for importing and open csv file
 
-def opencsv(filename):
-    dataset=list() # this is ap
-    with open(filename,'r') as file:
 
-        csv_read=read(file)
 
-        for row in csv_read:
+def lfile(filename):
+    dataset=list() # declaration of placeholder to store the dataset
+    with open(filename, 'r') as file:
+
+        data=reader(file)
+        for row in data:
             if not row:
                 continue
+
             dataset.append(row)
 
     return dataset
 
-filename='sonar.all-data.csv'
 
-opencsv(filename)
 
-#s
+file='/Users/mohendra/Dropbox/My_Projects/Test/sd.csv'
+
+L=lfile(file)
+
+print L
+
